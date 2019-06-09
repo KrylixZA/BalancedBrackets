7a13a821e6d512494a32ad9d73adb8808d73e053
Simon Headley <simon.headley@adaptit.co.za>
2019-06-09 18:46:02 +0200

    added C# stuff to the brackets checker repo

---
+++ BalancedBrackets/BalancedBrackets.Tests/BracketCheckerTests.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+
+namespace BalancedBrackets.Tests
+{
+    [TestFixture]
+    public class BracketCheckerTests
+    {
+        [Test]
+        public void TestAreBalanced_GivenEmptyString_ShouldReturnTrue()
+        {
+            // Arrange & Act
+            var balanced = BracketChecker.AreBalanced(string.Empty);
+
+            // Assert
+            Assert.IsTrue(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenOpenClosedBrackets_ShouldReturnTrue()
+        {
+            // Arrange
+            var input = "[]";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsTrue(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenSingleOpenBracket_ShouldReturnFalse()
+        {
+            // Arrange
+            var input = "[";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsFalse(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenSingleClosedBracket_ShouldReturnFalse()
+        {
+            // Arrange
+            var input = "]";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsFalse(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenDoubleOpenClosedBrackets_ShouldReturnTrue()
+        {
+            // Arrange
+            var input = "[][]";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsTrue(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenNestedOpenClosedBrackets_ShouldReturnTrue()
+        {
+            // Arrange
+            var input = "[[]]";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsTrue(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenMultipleNestedOpenClosedBrackets_ShouldReturnTrue()
+        {
+            // Arrange
+            var input = "[[[][]]]";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsTrue(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenCloseThenOpenBrackets_ShouldReturnFalse()
+        {
+            // Arrange
+            var input = "][";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsFalse(balanced);
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenClosedOpenClosedOpenBrackets_ShouldReturnFalse()
+        {
+            // Arrange
+            var input = "][][";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsFalse(balanced);
+            
+        }
+
+        [Test]
+        public void TestAreBalanced_GivenDoubleOpenClosedThenClosedOpenBrackets_ShouldReturnFalse()
+        {
+            // Arrange
+            var input = "[][]][";
+
+            // Act
+            var balanced = BracketChecker.AreBalanced(input);
+
+            // Assert
+            Assert.IsFalse(balanced);
+        }
+    }
+}
---
+++ BalancedBrackets/BalancedBrackets/BracketChecker.cs
@@ -0,0 +1,27 @@
+﻿namespace BalancedBrackets
+{
+    public static class BracketChecker
+    {
+        public static bool AreBalanced(string input)
+        {
+            if (input == "]")
+            {
+                return false;
+            }
+
+            var numOfPairs = 0;
+            foreach (var letter in input)
+            {
+                if (letter == '[')
+                {
+                    numOfPairs++;
+                } else if (letter == ']' && numOfPairs > 0)
+                {
+                    numOfPairs--;
+                }
+            }
+
+            return numOfPairs == 0;
+        }
+    }
+}
